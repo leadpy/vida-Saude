1613cb15e83ff60b9dcd9c0c5b4fdffe2c470660
Your Name <leadpytecnologia@gmail.com>
2023-08-02 10:52:14 -0300

    repositorio Oficial

---
+++ frontend/build/precache-manifest.c55422a57956a58f21dd66424d926038.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08f5b2c17bd63fec1e809ed143c53cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6de7fb7b9d3bd1b87ab8",
+    "url": "/static/css/2.fa426b8c.chunk.css"
+  },
+  {
+    "revision": "6de7fb7b9d3bd1b87ab8",
+    "url": "/static/js/2.5c7edeeb.chunk.js"
+  },
+  {
+    "revision": "334503b5ea74617d0ea6c9f43e18112f",
+    "url": "/static/js/2.5c7edeeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08640a1fe573e2c1f55c",
+    "url": "/static/js/3.e12b212d.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.e12b212d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d559b85c255cae3175b",
+    "url": "/static/js/main.709cc1da.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.709cc1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3921a61db0e17bc57c0a",
+    "url": "/static/js/runtime-main.deb96840.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "54822cf16f55a620faf00383a85f5c2f",
+    "url": "/static/media/logoLoginOption.54822cf1.png"
+  },
+  {
+    "revision": "54822cf16f55a620faf00383a85f5c2f",
+    "url": "/static/media/logoPainelOption.54822cf1.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  }
+]);
